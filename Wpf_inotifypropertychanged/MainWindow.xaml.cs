ccc1bae6d1aad77efed69aa685360db7b711b7a1
Michal Kuracina <michal.kuracina@henkel.com>
2021-12-06 15:47:13 +0100

    Add project files.

---
+++ Wpf_inotifypropertychanged/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Wpf_inotifypropertychanged.Model;
+
+namespace Wpf_inotifypropertychanged
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        CountModel countModel = new CountModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            counterLabel.DataContext = countModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            countModel.Counter++;
+        }
+    }
+}
---
+++ Wpf_inotifypropertychanged/Model/CountModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_inotifypropertychanged.Model
+{
+    public class CountModel : INotifyPropertyChanged
+    {
+        private int _counter;
+
+        public int Counter
+        {
+            get { return _counter; }
+            set
+            {
+                _counter = value;
+                OnPropertyChanged(nameof(Counter));
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
